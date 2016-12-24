c200ed165ef24eb6cd3a0912833ef8b93c621385
afkhero <raghava.vemuri@mavs.uta.edu>
2016-12-24 22:08:05 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,83 @@
+<?php
+ ob_start();
+ session_start();
+ require_once 'dbconnect.php';
+ 
+ // if session is not set this will redirect to login page
+ if( !isset($_SESSION['user']) ) {
+  header("Location: index.php");
+  exit;
+ }
+ // select loggedin users detail
+ $res=mysqli_query($conn, "SELECT * FROM users WHERE userId=".$_SESSION['user']);
+ $userRow=mysqli_fetch_array($res);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Welcome - <?php echo $userRow['userName']; ?></title>
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"  />
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+
+ <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="https://cse.uta.edu/">CSE 4316 FabLab Robot Team</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="https://fablab.uta.edu/">FabLab Home Page</a></li>
+            <li><a href="https://www.facebook.com/UTAFabLab/">FabLab Facebook Page</a></li>
+            <li><a href="https://fablab.uta.edu/about-fablab">Learn more about FabLab</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+     <span class="glyphicon glyphicon-user"></span>&nbsp;Hi,  <?php echo $userRow['userName']; ?>&nbsp;<span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="logout.php?logout"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Sign Out</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav> 
+
+ <div id="wrapper">
+
+ <div class="container">
+    
+     <div class="page-header">
+     <h3>Team Members: </h3>
+     </div>
+        
+        <div class="row">
+        <div class="col-lg-12">
+            <h1>Eyob Gemechu, <br> 
+            Eric Tran, <br> 
+            LeAnn Rasmussen, <br> 
+            Raghava Vemuri, <br> 
+            Sarah Varghese</h1> <br> 
+        </div>
+        </div>
+    
+    </div>
+    
+    </div>
+    
+    <script src="assets/jquery-1.11.3-jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    
+</body>
+</html>
+<?php ob_end_flush(); ?>
