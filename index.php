72c0aa9be56e48763f52f052d61efbff07e4d80a
afkhero <raghava.vemuri@mavs.uta.edu>
2016-12-26 01:00:20 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ and open the template in the editor.
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Coding Cage - Login & Registration System</title>
+<title>FabLab Robot Control Sign In Page</title>
 <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"  />
 <link rel="stylesheet" href="style.css" type="text/css" />
 </head>
